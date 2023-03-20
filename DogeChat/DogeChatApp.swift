2707e01ec37549faa739d791331a2c4efb8557a7
Steve Yu <steveyuowo@gmail.com>
2023-03-20 20:42:24 +0800

    Initial Commit

---
+++ DogeChat/DogeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogeChatApp.swift
+//  DogeChat
+//
+//  Created by Steve Yu on 2023/3/20.
+//
+
+import SwiftUI
+
+@main
+struct DogeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
